184323ef53528392eb016a05d0b43e51b3fd4f69
Siddharth Ramachandran <siddharth.r.ramachandran@oracle.com>
2024-12-30 17:53:50 +0530

    Add CompleteTripDto

--- common/src/main/java/com/payonefare/api/common/dto/CompleteTripDto.java
+++ common/src/main/java/com/payonefare/api/common/dto/CompleteTripDto.java
@@ -1,4 +1,22 @@
 package com.payonefare.api.common.dto;
 
+import io.micronaut.serde.annotation.Serdeable;
+
+/**
+ * DTO used for marking a trip as complete
+ */
+@Serdeable
 public class CompleteTripDto {
+    /*
+    Final Fare amount got from the driver
+     */
+    private float amount;
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
 }
