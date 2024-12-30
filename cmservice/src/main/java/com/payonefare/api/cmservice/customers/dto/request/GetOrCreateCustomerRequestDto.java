4130bb5bc0317ba30572f441b5df26cbf080b2e6
Siddharth Ramachandran <siddharth.r.ramachandran@oracle.com>
2024-12-30 16:24:22 +0530

    Create DTO for customer request object

---
+++ cmservice/src/main/java/com/payonefare/api/cmservice/customers/dto/request/GetOrCreateCustomerRequestDto.java
@@ -0,0 +1,39 @@
+package com.payonefare.api.cmservice.customers.dto.request;
+
+import io.micronaut.serde.annotation.Serdeable;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+@Serdeable
+public class GetOrCreateCustomerRequestDto {
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Pattern(regexp="(^$|[0-9]{10})")
+    private String phone;
+
+    public @NotNull String getName() {
+        return name;
+    }
+
+    public void setName(@NotNull String name) {
+        this.name = name;
+    }
+
+    public @NotNull @Pattern(regexp = "(^$|[0-9]{10})") String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(@NotNull @Pattern(regexp = "(^$|[0-9]{10})") String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "GetOrCreateCustomerRequestDto{" +
+                "name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
