5a7c1cd026acbfa5727c2dcd5b4a9d8b330e3b9e
Siddharth Ramachandran <siddharth.r.ramachandran@oracle.com>
2024-12-30 16:24:34 +0530

    Add client to communicate with dbgw

---
+++ cmservice/src/main/java/com/payonefare/api/cmservice/clients/DbgwClient.java
@@ -0,0 +1,20 @@
+package com.payonefare.api.cmservice.clients;
+
+import com.payonefare.api.cmservice.customers.dto.request.GetOrCreateCustomerRequestDto;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.client.annotation.Client;
+import jakarta.validation.Valid;
+
+@Client(id = "dbgw")
+public interface DbgwClient {
+
+    @Get("/customers/{phone}")
+    HttpResponse<Long> findCustomerByPhone(@PathVariable String phone);
+
+    @Post("/customers")
+    HttpResponse<Long> createCustomer(@Valid @Body GetOrCreateCustomerRequestDto getOrCreateCustomerRequestDto);
+}
